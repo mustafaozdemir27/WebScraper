dfe8e21ace8141657bae5f9e5a549a336b479813
byozdemr <42694496+byozdemr@users.noreply.github.com>
2020-12-25 16:47:36 +0300

    Initial commit

---
+++ Data/ScrapeCriteriaPart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebScraper.Data
+{
+    class ScrapeCriteriaPart
+    {
+        public string Regex { get; set; }
+        public RegexOptions RegexOption { get; set; }
+    }
+}
